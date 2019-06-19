d3362049e24bc1433ebbb176e7e5b61807e1d268
zhangrp <zhangrp@bairuitech.com>
2019-06-19 19:18:32 +0800

    again
    Change-Id: Ic58379a76073bf9a8ac6ac45fe51b98fb822fd0f

--- src/main.js
+++ src/main.js
@@ -5,14 +5,18 @@ import App from './App'
 import router from './router'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import fastClick from 'fastclick'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import 'styles/border.css'
 import 'styles/reset.css'
+// 因为大部分文件都会用到iconfont,所以在main.js引用
 import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
 
 Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-row>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  </el-row>
+</template>
+<script>
+
+</script>
+<style>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+     margin-bottom: 0;
+   }
+  }
+  .el-col {
+    /*border-radius: 4px;*/
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    /*border-radius: 4px;*/
+    height: .9rem
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
