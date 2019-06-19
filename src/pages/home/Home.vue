a61db2a59b46b3b0ef73e0dd17b3290861167fc2
zhangrp <zhangrp@bairuitech.com>
2019-06-19 19:13:41 +0800

    add components
    Change-Id: I7fc06ee7d2b926fcbe55b3033f16abc857e835a0

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,16 +2,19 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icon></home-icon>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcon from './components/Icons'
 export default {
-  name: 'List',
+  name: 'Home',
   components: {
     HomeHeader: HomeHeader,
-    HomeSwiper: HomeSwiper
+    HomeSwiper: HomeSwiper,
+    HomeIcon: HomeIcon
   }
 }
 </script>
