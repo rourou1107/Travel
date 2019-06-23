ab8a1c57bb77f7869db6d3d35d627464ecec55a1
zhangrp <zhangrp@bairuitech.com>
2019-06-23 18:06:31 +0800

    use better-scroll
    Change-Id: Ibbef6aa72c5dbd4d094d96f21fa6398d1fff031a

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+<script></script>
+<style lang="stylus" scoped>
+  @import '~styles/variable.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    right: 0
+    top: 1.48rem
+    bottom: 0
+    width: .4rem
+    .item
+      height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,68 +1,78 @@
 <template>
-  <div class="wrapper">
-    <div class="city-list border-topbottom">当前城市</div>
-    <div class="button-wrapper">
-      <div class="city-list-button">
-        <button class="button-list">北京</button>
+  <div class="wrapper" ref="wrapper"> <!--用于获取DOM元素-->
+    <div>
+      <div class="city-list border-topbottom">当前城市</div>
+      <div class="button-wrapper">
+        <div class="city-list-button">
+          <button class="button-list">北京</button>
+        </div>
       </div>
+      <div class="city-list border-topbottom">热门城市</div>
+      <div class="button-wrapper">
+        <div class="city-list-button">
+          <button class="button-list">北京</button>
+        </div>
+        <div class="city-list-button">
+          <button class="button-list">北京</button>
+        </div>
+        <div class="city-list-button">
+          <button class="button-list">北京</button>
+        </div>
+        <div class="city-list-button">
+          <button class="button-list">北京</button>
+        </div>
+        <div class="city-list-button">
+          <button class="button-list">北京</button>
+        </div>
+        <div class="city-list-button">
+          <button class="button-list">北京</button>
+        </div>
+        <div class="city-list-button">
+          <button class="button-list">北京</button>
+        </div>
+      </div>
+      <div class="city-list border-topbottom">A</div>
+      <ul>
+        <li class="city-capital border-bottom">阿拉尔</li>
+        <li class="city-capital border-bottom">阿拉尔</li>
+        <li class="city-capital border-bottom">阿拉尔</li>
+        <li class="city-capital border-bottom">阿拉尔</li>
+        <li class="city-capital border-bottom">阿拉尔</li>
+        <li class="city-capital border-bottom">阿拉尔</li>
+        <li class="city-capital border-bottom">阿拉尔</li>
+      </ul>
+      <div class="city-list border-topbottom">B</div>
+      <ul>
+        <li class="city-capital border-bottom">北京</li>
+        <li class="city-capital border-bottom">北京</li>
+        <li class="city-capital border-bottom">北京</li>
+        <li class="city-capital border-bottom">北京</li>
+        <li class="city-capital border-bottom">北京</li>
+        <li class="city-capital border-bottom">北京</li>
+        <li class="city-capital border-bottom">北京</li>
+      </ul>
+      <div class="city-list border-topbottom">C</div>
+      <ul>
+        <li class="city-capital border-bottom">成都</li>
+        <li class="city-capital border-bottom">成都</li>
+        <li class="city-capital border-bottom">成都</li>
+        <li class="city-capital border-bottom">成都</li>
+        <li class="city-capital border-bottom">成都</li>
+        <li class="city-capital border-bottom">成都</li>
+        <li class="city-capital border-bottom">成都</li>
+      </ul>
     </div>
-    <div class="city-list border-topbottom">热门城市</div>
-    <div class="button-wrapper">
-      <div class="city-list-button">
-        <button class="button-list">北京</button>
-      </div>
-      <div class="city-list-button">
-        <button class="button-list">北京</button>
-      </div>
-      <div class="city-list-button">
-        <button class="button-list">北京</button>
-      </div>
-      <div class="city-list-button">
-        <button class="button-list">北京</button>
-      </div>
-      <div class="city-list-button">
-        <button class="button-list">北京</button>
-      </div>
-      <div class="city-list-button">
-        <button class="button-list">北京</button>
-      </div>
-      <div class="city-list-button">
-        <button class="button-list">北京</button>
-      </div>
-    </div>
-    <div class="city-list border-topbottom">A</div>
-    <ul>
-      <li class="city-capital border-bottom">阿拉尔</li>
-      <li class="city-capital border-bottom">阿拉尔</li>
-      <li class="city-capital border-bottom">阿拉尔</li>
-      <li class="city-capital border-bottom">阿拉尔</li>
-      <li class="city-capital border-bottom">阿拉尔</li>
-      <li class="city-capital border-bottom">阿拉尔</li>
-      <li class="city-capital border-bottom">阿拉尔</li>
-    </ul>
-    <div class="city-list border-topbottom">B</div>
-    <ul>
-      <li class="city-capital border-bottom">北京</li>
-      <li class="city-capital border-bottom">北京</li>
-      <li class="city-capital border-bottom">北京</li>
-      <li class="city-capital border-bottom">北京</li>
-      <li class="city-capital border-bottom">北京</li>
-      <li class="city-capital border-bottom">北京</li>
-      <li class="city-capital border-bottom">北京</li>
-    </ul>
-    <div class="city-list border-topbottom">C</div>
-    <ul>
-      <li class="city-capital border-bottom">成都</li>
-      <li class="city-capital border-bottom">成都</li>
-      <li class="city-capital border-bottom">成都</li>
-      <li class="city-capital border-bottom">成都</li>
-      <li class="city-capital border-bottom">成都</li>
-      <li class="city-capital border-bottom">成都</li>
-      <li class="city-capital border-bottom">成都</li>
-    </ul>
   </div>
 </template>
-<script></script>
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
 <style lang="stylus" scoped>
   .wrapper
     position: absolute
