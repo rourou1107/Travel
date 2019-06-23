7ff4026955caef61fdaa6298fb51c8a96fc5af8c
zhangrp <zhangrp@bairuitech.com>
2019-06-23 17:21:16 +0800

    add city list
    Change-Id: I8b0245c1b0839595bc105cc6c759025b2e5dd55f

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="wrapper">
+    <div class="city-list border-topbottom">当前城市</div>
+    <div class="button-wrapper">
+      <div class="city-list-button">
+        <button class="button-list">北京</button>
+      </div>
+    </div>
+    <div class="city-list border-topbottom">热门城市</div>
+    <div class="button-wrapper">
+      <div class="city-list-button">
+        <button class="button-list">北京</button>
+      </div>
+      <div class="city-list-button">
+        <button class="button-list">北京</button>
+      </div>
+      <div class="city-list-button">
+        <button class="button-list">北京</button>
+      </div>
+      <div class="city-list-button">
+        <button class="button-list">北京</button>
+      </div>
+      <div class="city-list-button">
+        <button class="button-list">北京</button>
+      </div>
+      <div class="city-list-button">
+        <button class="button-list">北京</button>
+      </div>
+      <div class="city-list-button">
+        <button class="button-list">北京</button>
+      </div>
+    </div>
+    <div class="city-list border-topbottom">A</div>
+    <ul>
+      <li class="city-capital border-bottom">阿拉尔</li>
+      <li class="city-capital border-bottom">阿拉尔</li>
+      <li class="city-capital border-bottom">阿拉尔</li>
+      <li class="city-capital border-bottom">阿拉尔</li>
+      <li class="city-capital border-bottom">阿拉尔</li>
+      <li class="city-capital border-bottom">阿拉尔</li>
+      <li class="city-capital border-bottom">阿拉尔</li>
+    </ul>
+    <div class="city-list border-topbottom">B</div>
+    <ul>
+      <li class="city-capital border-bottom">北京</li>
+      <li class="city-capital border-bottom">北京</li>
+      <li class="city-capital border-bottom">北京</li>
+      <li class="city-capital border-bottom">北京</li>
+      <li class="city-capital border-bottom">北京</li>
+      <li class="city-capital border-bottom">北京</li>
+      <li class="city-capital border-bottom">北京</li>
+    </ul>
+    <div class="city-list border-topbottom">C</div>
+    <ul>
+      <li class="city-capital border-bottom">成都</li>
+      <li class="city-capital border-bottom">成都</li>
+      <li class="city-capital border-bottom">成都</li>
+      <li class="city-capital border-bottom">成都</li>
+      <li class="city-capital border-bottom">成都</li>
+      <li class="city-capital border-bottom">成都</li>
+      <li class="city-capital border-bottom">成都</li>
+    </ul>
+  </div>
+</template>
+<script></script>
+<style lang="stylus" scoped>
+  .wrapper
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    overflow: hidden
+    .border-topbottom
+      &:before
+        border-color: #777
+      &:after
+       border-color: #777
+    .city-list
+      height: .44rem
+      line-height: .44rem
+      background-color: #eee
+      padding-left: .2rem
+    .button-wrapper
+      display: flex
+      flex-wrap: wrap
+      .city-list-button
+        box-sizing: border-box
+        width: 2.2rem
+        height: .9rem
+        /*line-height: 1rem*/
+        padding: .19rem .2rem
+        .button-list
+          width: 1.8rem
+          height: .52rem
+          border-radius: .06rem
+          border: 1px solid #ccc
+          background-color: #fff
+    .city-capital
+      height: .7rem
+      line-height: .7rem
+      padding-left: .2rem
+</style>
