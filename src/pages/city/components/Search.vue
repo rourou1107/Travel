d9b99493155aa2706ff06e37dcf7bc46e1519b49
zhangrp <zhangrp@bairuitech.com>
2019-06-27 15:19:40 +0800

    city search
    Change-Id: I5d12d10fc7891f0027b2bd1b379591652075de00

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,11 +1,75 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  <div>
+    <div class="search">
+      <input
+        class="search-input" type="text"
+        placeholder="输入城市名或拼音"
+        v-model="keyword"
+      />
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-written border-bottom"
+            v-for="item of list"
+            :key="item.id"
+        >
+          {{item.name}}
+        </li>
+        <li
+          class="search-written border-bottom"
+          v-if="hasNoData"
+        >
+          输入的城市不存在,请重新输入
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      list: [],
+      keyword: '',
+      timer: false
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let key in this.cities) {
+          this.cities[key].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -22,4 +86,18 @@ export default {
       color: #666
       padding: 0 .1rem
       box-sizing: border-box
+  .search-content
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    overflow: hidden
+    background-color: #e5e9f4
+    z-index: 1
+    .search-written
+      height: .54rem
+      line-height: .54rem
+      padding-left: .2rem
+      background-color: #fff
 </style>
