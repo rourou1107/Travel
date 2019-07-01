db2811c4fab89de808b364c22fbdafd28085ead3
zhangrp <zhangrp@bairuitech.com>
2019-07-01 15:54:52 +0800

    banner区域 img点击放大效果
    Change-Id: I63925d1f7d61cd1c71ced1b17f9b5e0ae65a4ac9

---
+++ src/pages/common/gallery/Gallery.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container" @click="handleGalleryClick">
+    <swiper :options="swiperOption" class="wrapper">
+      <swiper-slide v-for="(item, index) of imgUrl" :key="index">
+        <img class='gallery-img' :src="item"/>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'gallery',
+  props: ['imgUrl'],
+  data () {
+    return {
+      swiperOption: {
+        autoplay: true, // 自动滚动
+        loop: true, // 支持循环轮播
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGalleryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .container >>> .swiper-pagination-fraction
+    bottom: 1rem
+  .container
+    z-index: 99
+    overflow: hidden
+    position: absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    background-color: #000
+    display: flex
+    flex-direction: column
+    justify-content: center
+    .wrapper
+      width: 100%
+      height: 0
+      padding-bottom: 100%
+      .gallery-img
+        width: 100%
+    .swiper-pagination
+      color: #fff
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="banner">
+    <div class="banner" @click="handleBannerClick">
       <img class="banner-img"
            src="//img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg"
       />
@@ -12,11 +12,38 @@
         </p>
       </div>
     </div>
+    <banner-gallery
+      :imgUrl="imgUrl"
+      v-show="showSelf"
+      @close="close"
+    ></banner-gallery>
   </div>
 </template>
 <script>
+import BannerGallery from 'common/gallery/Gallery'
 export default {
-  name: 'Banner'
+  name: 'Banner',
+  data () {
+    return {
+      imgUrl: [
+        'http://img1.qunarzz.com/sight/p0/1904/9c/9cfe77cf4b5e7f58a3.img.jpg_350x240_2737a5a8.jpg',
+        'http://img1.qunarzz.com/sight/p0/1605/d4/d4e2dce9d328d5b290.img.jpg_350x240_07d6e292.jpg',
+        'http://img1.qunarzz.com/sight/p0/1412/32/96b22e5c194d4bcb9458f29fbdc45d57.water.jpg_350x240_ac44becd.jpg'
+      ],
+      showSelf: false
+    }
+  },
+  components: {
+    BannerGallery
+  },
+  methods: {
+    handleBannerClick () {
+      this.showSelf = true
+    },
+    close () {
+      this.showSelf = false
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
